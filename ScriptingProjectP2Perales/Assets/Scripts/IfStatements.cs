c3b7666509cd582b3aeb9006cb9be64a3ad5e983
NP36752 <735430@student.dusd.net>
2024-10-28 09:26:38 -0700

    did loops

--- ScriptingProjectP2Perales/Assets/Scripts/IfStatements.cs
+++ ScriptingProjectP2Perales/Assets/Scripts/IfStatements.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class IfStatements : MonoBehaviour
 {
     float coffeeTemperature = 85.0f;
     float hotLimitTemperature = 70.0f;
